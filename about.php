a72adcca92f7a0f51baa59424de096dd6ee71c04
Azad Kolachi <azaadone@gmail.com>
2014-12-28 14:42:43 +0500

    basic scaffold for website complete

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Azad -  Bootstrap3 - ReadyMade -- Home</title>
+    <title>Azad -  Bootstrap3 - ReadyMade -- About Us</title>
 	<link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link href="assets/css/bootstrap.css" rel="stylesheet">
     <link href="assets/css/mystyles.css" rel="stylesheet">
@@ -16,7 +16,7 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body id="home">
+  <body id="about">
   
   
   <!-- .container -->
