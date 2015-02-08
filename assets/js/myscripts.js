b4cef8330ac8be3194f461f4ca003d27e9607b62
MuhammadAzad <azaadone@gmail.com>
2015-02-08 23:50:20 +0500

    Grunt and JSON

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -1,1 +1,34 @@
 $(function(){$('#home a:contains("Home")').parent().addClass("active"),$('#about a:contains("About Us")').parent().addClass("active"),$('#services a:contains("Services")').parent().addClass("active"),$('#faqs a:contains("FAQs")').parent().addClass("active"),$('#contact a:contains("Contact Us")').parent().addClass("active"),$("ul.nav li.dropdown").hover(function(){$(".dropdown-menu",this).fadeIn()},function(){$(".dropdown-menu",this).fadeOut("fast")})});
+
+
+
+function Validator()
+{
+    "use strict";
+}
+
+Validator.prototype.checkName = function(name)
+{
+    "use strict";
+    return (/[^a-z]/i.test(name) === false);
+};
+
+window.addEventListener('load', function(){
+    "use strict";
+    document.getElementById('firstname').addEventListener('blur', function(){
+        var _this = this;
+        var validator = new Validator();
+        var validation = document.getElementById('namevalidation');
+        if (validator.checkName(_this.value) === true) {
+            validation.innerHTML = 'Looks good! :)';
+            validation.className = "validation yep";
+            _this.className = "yep";
+        }
+        else {
+            validation.innerHTML = 'Looks bad! :(';
+            validation.className = "validation nope";
+            _this.className = "nope";
+        }
+
+    });
+});
